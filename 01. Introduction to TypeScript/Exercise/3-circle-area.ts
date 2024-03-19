7ff25f9853c259c0aa03087243fe91fccff3bd96
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-03-19 11:57:28 +0200

    ex-2

--- 01. Introduction to TypeScript/Exercise/3-circle-area.ts
+++ 01. Introduction to TypeScript/Exercise/3-circle-area.ts
@@ -1,3 +1,13 @@
-function area() {
+function area(input: string | number): void {
+    let area: number = 0;
 
+    if (typeof input !== 'number') {
+        `We can not calculate the circle area, because we receive a ${typeof input}.`
+    } else {
+        area = Math.PI * input ** 2;
+    }
+
+    console.log(area.toFixed(2));
 }
+
+area(5)
