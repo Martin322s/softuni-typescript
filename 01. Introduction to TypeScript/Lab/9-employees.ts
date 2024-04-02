350fc5e3144c4273b897cf656838060e091314b1
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-04-02 16:14:57 +0300

    ex9-employees

---
+++ 01. Introduction to TypeScript/Lab/9-employees.ts
@@ -0,0 +1,20 @@
+function employees(input: string[]): void {
+    let result: { name: string, personalNumber: number }[] = [];
+
+    for (let person of input) {
+        result.push({ name: person, personalNumber: person.length });
+    }
+
+    result
+        .forEach((person) =>
+            console.log(`Name: ${person.name} -- Personal Number: ${person.personalNumber}`)
+        );
+}
+
+employees([
+    'Samuel Jackson',
+    'Will Smith',
+    'Bruce Willis',
+    'Tom Holland'
+    ]
+    );
