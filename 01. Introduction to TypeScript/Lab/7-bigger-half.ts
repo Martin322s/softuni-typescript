f5ea6c99698de4354d7f7dc7026b0dde812b0597
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-04-02 15:53:52 +0300

    ex6-bigger-half

--- 01. Introduction to TypeScript/Lab/7-bigger-half.ts
+++ 01. Introduction to TypeScript/Lab/7-bigger-half.ts
@@ -1,3 +1,13 @@
-function biggerHalf() {
+function biggerHalf(arr: number[]): void {
+    let resultArr: number[] = [];
+    let sortedArr: number[] = arr.sort((a: number, b: number) => a - b);
+    let startNumber = sortedArr.length % 2 === 0 
+            ? Math.ceil(sortedArr.length / 2) 
+            : Math.ceil(sortedArr.length / 2) - 1;
     
+    for (let i: number = startNumber; i < sortedArr.length; i++) {
+        resultArr.push(sortedArr[i]);
+    }
+
+    console.log(resultArr);
 }
