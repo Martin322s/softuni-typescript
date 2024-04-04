8ed47f2114bea72c22f53724f680e7e543746949
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-04-05 00:04:03 +0300

    ex-5

---
+++ 02. Advanced Data Types and Interfaces/Lab/6-lowest-prices.ts
@@ -0,0 +1,44 @@
+type Product = {
+    name: string,
+    price: number,
+    town: string
+};
+
+function lowestPrices(input: string[]) {
+    let result: Product[] = [];
+    let lowestPriceProducts: Product[] | any[] = [];
+
+    for (let product of input) {
+        let [town, name, priceAsString] = product.split(' | ');
+        let price = Number(priceAsString);
+
+        let productObj: Product = { town, name, price };
+        result.push(productObj);
+    }
+
+    for (let product of result) {
+        if (!lowestPriceProducts.some(x => x.name === product.name)) {
+            lowestPriceProducts.push(product);
+        } else {
+            let element = lowestPriceProducts.find(x => x.name === product.name);
+            let index = lowestPriceProducts.indexOf(element);
+
+            if (element.price > product.price) {
+                lowestPriceProducts.splice(index, 1, product);
+            }
+        }
+    }
+
+    lowestPriceProducts.forEach(x => console.log(`${x.name} -> ${x.price} (${x.town})`));
+}
+
+lowestPrices([
+    'Sample Town | Sample Product | 1000',
+    'Sample Town | Orange | 2',
+    'Sample Town | Peach | 1',
+    'Sofia | Orange | 3',
+    'Sofia | Peach | 2',
+    'New York | Sample Product | 1000.1',
+    'New York | Burger | 10',
+    'New York | Sample Product | 999',
+]);
--- 02. Advanced Data Types and Interfaces/Lab/build/5-towns-population.js
+++ 02. Advanced Data Types and Interfaces/Lab/build/5-towns-population.js
@@ -15,11 +15,3 @@ function townPopulation(input) {
         console.log(`${key} : ${resultObj[key]}`);
     }
 }
-townPopulation([
-    'Sofia <-> 1200000',
-    'Montana <-> 20000',
-    'New York <-> 10000000',
-    'Washington <-> 2345000',
-    'Las Vegas <-> 1000000',
-    'Montana <-> 1'
-]);
