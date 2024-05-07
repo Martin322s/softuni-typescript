db656f9a786305108f8766f587d89fa3745b01a7
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-05-07 18:34:55 +0300

    intersection type

--- Trainings/2-advanced-types.ts
+++ Trainings/2-advanced-types.ts
@@ -1,3 +1,19 @@
-// Union type
+// Union type - variable which can be either this type or this exmp: string | number
 let variable: string | number = 'Value';
 variable = 12;
+
+// Intersection type - combine two types in one
+type Cat = {
+    furColor: string;
+    legsCount: number;
+}
+
+type Animal = {
+    walking: () => void
+}
+
+let garry: Cat & Animal = { 
+    furColor: 'Orange', 
+    legsCount: 4, 
+    walking: () => console.log('Move') 
+};
