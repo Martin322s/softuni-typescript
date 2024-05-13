aa06ed498e7c76212ea9204546e82b8799c9c08c
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-05-13 15:29:21 +0300

    factory design pattern

--- Trainings/4-generics.ts
+++ Trainings/4-generics.ts
@@ -3,7 +3,7 @@
 class Singleton {
     private static instance: Singleton | null = null;
 
-    private constructor() {}
+    private constructor() { }
 
     static getInstance(): Singleton | null {
         if (this.instance === null) {
@@ -18,3 +18,39 @@ let instance1 = Singleton.getInstance();
 let instance2 = Singleton.getInstance();
 
 console.log(instance1 === instance2);
+
+// 2. Factory pattern - function, which creates instances
+class CarFactory {
+    brand: string;
+
+    constructor(brand: string) {
+        this.brand = brand;
+    }
+
+    getBrand(): string {
+        return this.brand;
+    }
+}
+
+class BMWCar extends CarFactory {
+    constructor(brand: string) {
+        super(brand);
+    }
+}
+
+class OpelCar extends CarFactory {
+    constructor(brand: string) {
+        super(brand);
+    }
+}
+
+function factory(brand: string) {
+    switch (brand) {
+        case 'BMW': return new BMWCar(brand);
+        case 'Opel': return new OpelCar(brand);
+        default: throw new Error('Car type not supported!');
+    }
+}
+
+console.log(factory('BMW'));
+console.log(factory('Opel'));
