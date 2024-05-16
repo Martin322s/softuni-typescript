63b393a74755c2d2b5b55abcb01cf4bf503ebe1c
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-05-16 15:56:50 +0300

    method decorator

--- Trainings/6-decorators.ts
+++ Trainings/6-decorators.ts
@@ -1,6 +1,11 @@
 // Basic Decorator
 function classDecorator(constructor: Function) {
-    console.log('Decoraator called!');
+    console.log('Decorator called!');
+}
+
+function methodDecorator(target: Emitter, key: String, descriptor: TypedPropertyDescriptor<() => void>) {
+    console.log('Method decorator called!');
+    
 }
 
 @classDecorator
@@ -8,8 +13,14 @@ class Emitter {
     constructor() {
         console.log('Constructor called!');
     }
+
+    @methodDecorator
+    emit() {
+        console.log('Method called');
+    }
 }
 
 const emit = new Emitter();
 
 console.log(emit);
+emit.emit();
