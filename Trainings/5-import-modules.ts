68fc2a50d60e3f21468301d244c6f8c69cb2c4c0
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-05-16 20:10:32 +0300

    named module and defualt export

--- Trainings/5-import-modules.ts
+++ Trainings/5-import-modules.ts
@@ -1,5 +1,5 @@
 import { sum } from './5-export-modules';
-// import calculator from './5-export-modules';
+import calculator from './5-export-modules';
 
-// console.log(sum(1, 2));
-// console.log(calculator.subtract(3, 2));
+console.log(sum(1, 2));
+console.log(calculator.subtract(3, 2));
