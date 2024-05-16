34943d05cc6e46b703fc59e00030202366fc5035
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-05-16 20:13:47 +0300

    alias import

--- Trainings/5-import-modules.ts
+++ Trainings/5-import-modules.ts
@@ -1,5 +1,6 @@
 import { sum } from './5-export-modules';
-import calculator from './5-export-modules';
+import  * as calculator  from './5-export-modules';
 
 console.log(sum(1, 2));
 console.log(calculator.subtract(3, 2));
+console.log(calculator.default);
