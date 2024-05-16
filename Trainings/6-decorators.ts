462924528ba1a5442cf930703f6c320e36f4b46e
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-05-16 16:02:08 +0300

    accessor decorators

--- Trainings/6-decorators.ts
+++ Trainings/6-decorators.ts
@@ -5,7 +5,7 @@ function classDecorator(constructor: Function) {
 
 function methodDecorator(target: Emitter, key: String, descriptor: TypedPropertyDescriptor<() => void>) {
     console.log('Method decorator called!');
-    
+
 }
 
 @classDecorator
@@ -24,3 +24,23 @@ const emit = new Emitter();
 
 console.log(emit);
 emit.emit();
+
+
+function configurable(configurable: boolean) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        descriptor.configurable = configurable;
+    }
+}
+
+class Point2 {
+    private _x: number;
+    private _y: number;
+    constructor(x: number, y: number) {
+        this._x = x;
+        this._y = y;
+    }
+    @configurable(false)
+    get x() { return this._x; }
+    @configurable(false)
+    get y() { return this._y; }
+}
