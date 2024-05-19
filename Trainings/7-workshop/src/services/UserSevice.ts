b687a1741c4b56a324b5e1955f6fc9f1aa742f24
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-05-19 12:32:13 +0300

    create user

--- Trainings/7-workshop/src/services/UserSevice.ts
+++ Trainings/7-workshop/src/services/UserSevice.ts
@@ -5,4 +5,16 @@ export class UserService<T> {
         const res = await fetch(`${BASE_URL}/users`);
         return await res.json();
     }
+
+    async createUser(firstName: string, lastName: string) {
+        const res = await fetch(`${BASE_URL}/users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ firstName, lastName });
+        });
+        
+        return await res.json();
+    }
 }
